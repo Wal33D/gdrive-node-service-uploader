041cf721980d99a68c76308e816cfd20a3764884
Waleed Gudah <aquataze@yahoo.com>
2024-05-30 10:14:18 -0500

    add types export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { uploadFileToGoogleDrive } from './fileUpload';
 export { deleteFileFromGoogleDrive } from './fileDelete';
 export { downloadFileFromGoogleDrive } from './fileDownload';
+export { UploadFileParams, DownloadFileParams, DeleteFileParams } from './types';

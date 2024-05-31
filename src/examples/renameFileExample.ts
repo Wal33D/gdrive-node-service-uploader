2636188b99e9478e3894bf59799dbec7780f9214
Waleed Gudah <aquataze@yahoo.com>
2024-05-31 17:20:33 -0500

    Refactor renameFileExample.ts for clarity and readability

--- src/examples/renameFileExample.ts
+++ src/examples/renameFileExample.ts
@@ -3,12 +3,12 @@ import { renameFileInGoogleDrive } from '../fileRename';
 const fileId = '1y2E2o7Jf_uh4OI4KsPjL7HbqkJRiOtxO';
 const fileUrl = 'https://drive.google.com/file/d/1HRmETDPMHlHtHpIZCMJP0JjB9AeX_PgU/view?usp=drivesdk';
 const fileName = 'renamed_example_archive.zip';
-const newName = 'renamed_again_example_archive.zip';
+const newName = 'ManicMinersV0.9.zip';
 
 const renameFileExample = async () => {
    try {
       // Example using fileName
-      const resultByName = await renameFileInGoogleDrive({ fileName, newName });
+      const resultByName = await renameFileInGoogleDrive({ fileId, newName });
       console.log(resultByName);
    } catch (error) {
       console.error(`Error: ${(error as Error).message}`);

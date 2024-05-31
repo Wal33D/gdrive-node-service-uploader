e627e2e810cf134e0cadeaa765fe02490f0babd4
Waleed Gudah <aquataze@yahoo.com>
2024-05-31 16:55:30 -0500

    Refactor renameFileExample.ts for better clarity and maintainability

--- src/examples/renameFileExample.ts
+++ src/examples/renameFileExample.ts
@@ -1,6 +1,6 @@
 import { renameFileInGoogleDrive } from '../fileRename';
 
-const fileId = '1HRmETDPMHlHtHpIZCMJP0JjB9AeX_PgU';
+const fileId = '1y2E2o7Jf_uh4OI4KsPjL7HbqkJRiOtxO';
 const fileUrl = 'https://drive.google.com/file/d/1HRmETDPMHlHtHpIZCMJP0JjB9AeX_PgU/view?usp=drivesdk';
 const fileName = 'renamed_example_archive.zip';
 const newName = 'renamed_again_example_archive.zip';
